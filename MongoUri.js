92a6fdcce88d6e429d79f81d4b01e0f39073b366
MostafaKolkela <115589891+MostafaKolkela@users.noreply.github.com>
2024-05-14 20:25:08 +0300

    final

---
+++ MongoUri.js
@@ -0,0 +1,4 @@
+mongoUri='mongodb+srv://moustafakolkailaofficial:Mostafa25102003@cluster0.qnja7yk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+module.exports=mongoUri
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const  mongoose = require('mongoose');
+const app = express();
+const MONGODB_URI = require('./MongoUri')
+const orderRouter = require('./router/orderRouter');
+const productRouter = require('./router/productRouter');
+const usersRouter = require('./router/userRouter');
+
+app.use(express.json());
+
+app.use('/api/users', usersRouter) 
+
+app.use('/api/orders', orderRouter) 
+
+app.use('/api/products', productRouter) 
+
+mongoose.connect(MONGODB_URI)
+.then(()=>{
+   app.listen(3000 , ()=>{
+      console.log("started on port 3000")
+   })
+})
+.catch(err=>{
+   console.log(err)
+})
+
